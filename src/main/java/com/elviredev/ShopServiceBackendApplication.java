409d5e3e31e228515c028c67c36aae56edd0fb9e
elviredev <sandrine.rodriguez@hotmail.com>
2019-09-15 16:48:32 +0200

    Entities Product & Category
    Package -> com.elviredev.entities

--- src/main/java/com/elviredev/ShopServiceBackendApplication.java
+++ src/main/java/com/elviredev/ShopServiceBackendApplication.java
@@ -1,4 +1,4 @@
-package com.elviredev.shopservicebackend;
+package com.elviredev;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
