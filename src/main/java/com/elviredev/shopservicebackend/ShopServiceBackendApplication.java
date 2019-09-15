388030571498b8f81c64bf010b2f78d2475a60f8
elviredev <sandrine.rodriguez@hotmail.com>
2019-09-15 16:07:47 +0200

    Initial commit

---
+++ src/main/java/com/elviredev/shopservicebackend/ShopServiceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.elviredev.shopservicebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopServiceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopServiceBackendApplication.class, args);
+	}
+
+}
