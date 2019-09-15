7f0181a0e288737237e00a03855e0480fffb4606
elviredev <sandrine.rodriguez@hotmail.com>
2019-09-15 17:57:22 +0200

    ShopServiceBackendApplication et application.properties

--- src/main/java/com/elviredev/ShopServiceBackendApplication.java
+++ src/main/java/com/elviredev/ShopServiceBackendApplication.java
@@ -1,13 +1,34 @@
 package com.elviredev;
 
+import com.elviredev.dao.CategoryRepository;
+import com.elviredev.dao.ProductRepository;
+
+import com.elviredev.entities.Category;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.ArrayList;
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class ShopServiceBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShopServiceBackendApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner start(CategoryRepository categoryRepository, ProductRepository productRepository){
+		return args -> {
+			categoryRepository.deleteAll();
+			Stream.of("C1 Ordinateurs", "C2 Imprimantes").forEach(c -> {
+				categoryRepository.save(new Category(c.split(" ")[0], c.split(" ")[1], new ArrayList<>()));
+			});
+
+			categoryRepository.findAll().forEach(System.out::println); // appelle la méthode toString() pour chaque objet category
+		};
 	}
 
 }
