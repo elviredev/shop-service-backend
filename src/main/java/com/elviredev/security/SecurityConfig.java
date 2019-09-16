723c6438ab11801fb6fb51b95ebd4a8c6e4f3800
elviredev <sandrine.rodriguez@hotmail.com>
2019-09-16 15:28:00 +0200

    SecurityConfig.java -> auth STATELESS
    basée sur TOKEN JWT et non plus sur les sessions

--- src/main/java/com/elviredev/security/SecurityConfig.java
+++ src/main/java/com/elviredev/security/SecurityConfig.java
@@ -7,28 +7,25 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-       BCryptPasswordEncoder bcpe = getBCPE();
-       auth.inMemoryAuthentication().withUser("admin").password(bcpe.encode("1234")).roles("ADMIN", "USER");
-       auth.inMemoryAuthentication().withUser("user1").password(bcpe.encode("1234")).roles("USER");
-    }
+
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        //super.configure(http); // config par défaut
         http.csrf().disable(); // desactiver le csrf token
-        //http.authorizeRequests().anyRequest().permitAll();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.authorizeRequests().antMatchers("/categories/**").hasAuthority("ADMIN");
+        http.authorizeRequests().antMatchers("/products/**").hasAuthority("USER");
+        http.authorizeRequests().anyRequest().authenticated();
+        http.addFilterBefore(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 
-    @Bean
-    public BCryptPasswordEncoder getBCPE(){
-        return new BCryptPasswordEncoder();
-    }
+
 }
