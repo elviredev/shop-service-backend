a186872845e656387a7228289d8c548f647a0b87
elviredev <sandrine.rodriguez@hotmail.com>
2019-09-16 11:38:54 +0200

    Modif Category.java => toString()
    Suppression @toString() Lombok
    rajout de toString() à la main sans la propriété products pour éviter boucle infinie avec relation bi-directionnelle category/product en BDD

--- src/main/java/com/elviredev/entities/Category.java
+++ src/main/java/com/elviredev/entities/Category.java
@@ -3,7 +3,7 @@ package com.elviredev.entities;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.ToString;
+
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -12,11 +12,19 @@ import java.util.ArrayList;
 import java.util.Collection;
 
 @Document
-@Data @AllArgsConstructor @NoArgsConstructor @ToString
+@Data @AllArgsConstructor @NoArgsConstructor
 public class Category {
     @Id
     private String id;
     private String name;
     @DBRef
     private Collection<Product> products = new ArrayList<>();
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
