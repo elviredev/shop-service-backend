38bb434a9d59a720ce0c8a507781994e56d9308b
elviredev <sandrine.rodriguez@hotmail.com>
2019-09-16 11:39:57 +0200

    Update ShopServiceBackendApplication.java
    Ajout des produits en BDD

--- src/main/java/com/elviredev/ShopServiceBackendApplication.java
+++ src/main/java/com/elviredev/ShopServiceBackendApplication.java
@@ -4,6 +4,7 @@ import com.elviredev.dao.CategoryRepository;
 import com.elviredev.dao.ProductRepository;
 
 import com.elviredev.entities.Category;
+import com.elviredev.entities.Product;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -28,6 +29,29 @@ public class ShopServiceBackendApplication {
 			});
 
 			categoryRepository.findAll().forEach(System.out::println); // appelle la méthode toString() pour chaque objet category
+
+			// supprime les données avant chaque lancement de l'appli
+			productRepository.deleteAll();
+			// récupère la catégorie c1
+			Category c1 = categoryRepository.findById("C1").get();
+			// parcourt et enregistre les données
+			Stream.of("P1", "P2", "P3", "P4").forEach(name -> {
+				Product p = productRepository.save(new Product(null, name, Math.random()*1000, c1));
+				c1.getProducts().add(p);
+				categoryRepository.save(c1);
+			});
+
+			// récupère la catégorie c2
+			Category c2 = categoryRepository.findById("C2").get();
+			Stream.of("P5", "P6").forEach(name -> {
+				Product p = productRepository.save(new Product(null, name, Math.random()*1000, c2));
+				c2.getProducts().add(p);
+				categoryRepository.save(c2);
+			});
+
+			productRepository.findAll().forEach(p -> {
+				System.out.println(p.toString());
+			});
 		};
 	}
 
