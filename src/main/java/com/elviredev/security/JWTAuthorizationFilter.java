f8c716293d0192ab74bc13f0cf47a5041d4ce325
elviredev <sandrine.rodriguez@hotmail.com>
2019-09-17 11:08:58 +0200

    JWTAuthorizationFilter
    Remarque : la sécurité va faire l'objet d'un micro-service spécique "security-service" avec  authentification Stateless, BDD SQL - JPA, gestion des utilisateurs et des rôles via Spring Data

---
+++ src/main/java/com/elviredev/security/JWTAuthorizationFilter.java
@@ -0,0 +1,26 @@
+package com.elviredev.security;
+
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class JWTAuthorizationFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
+        System.out.println("****************************");
+        /*
+
+        */
+        String jwt = httpServletRequest.getHeader("Authorization");
+        if(jwt == null) throw new RuntimeException("Not Authorized");
+        /*
+
+         */
+        // sinon
+        filterChain.doFilter(httpServletRequest, httpServletResponse);
+    }
+}
--- src/main/java/com/elviredev/security/SecurityConfig.java
+++ src/main/java/com/elviredev/security/SecurityConfig.java
@@ -1,21 +1,15 @@
 package com.elviredev.security;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -26,6 +20,4 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.authorizeRequests().anyRequest().authenticated();
         http.addFilterBefore(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
     }
-
-
 }
