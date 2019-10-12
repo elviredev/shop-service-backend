d13c8b0543b98892882377ee865344142e973194
elviredev <sandrine.rodriguez@hotmail.com>
2019-10-12 10:43:26 +0200

    Delete CategoryRepository.java

--- src/main/java/com/elviredev/dao/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.elviredev.dao;
-
-import com.elviredev.entities.Category;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-@RepositoryRestResource
-public interface CategoryRepository extends MongoRepository<Category, String> {
-}
