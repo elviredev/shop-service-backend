933c704d90429b258c8cdec328f44773bd8e8a17
elviredev <sandrine.rodriguez@hotmail.com>
2019-10-12 10:43:37 +0200

    Create ContentAppli.java

---
+++ src/main/java/com/elviredev/entities/ContentAppli.java
@@ -0,0 +1,22 @@
+package com.elviredev.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+
+import java.io.Serializable;
+
+@Document
+@Data @AllArgsConstructor @NoArgsConstructor @ToString
+public class ContentAppli implements Serializable {
+    @Id
+    private String idContent;
+    private String contentName;
+    @DBRef
+    private CoachedAppli coachedAppli;
+}
