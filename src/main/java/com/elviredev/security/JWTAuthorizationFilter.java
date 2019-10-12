c8bf864937265587bd5016e0658dd1c7b2604c41
elviredev <sandrine.rodriguez@hotmail.com>
2019-10-12 10:42:52 +0200

    Update JWTAuthorizationFilter.java

--- src/main/java/com/elviredev/security/JWTAuthorizationFilter.java
+++ src/main/java/com/elviredev/security/JWTAuthorizationFilter.java
@@ -1,5 +1,13 @@
 package com.elviredev.security;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -7,20 +15,52 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 public class JWTAuthorizationFilter extends OncePerRequestFilter {
     @Override
-    protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
-        System.out.println("****************************");
-        /*
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        response.addHeader("Access-Control-Allow-Origin", "*");
+        response.addHeader("Access-Control-Allow-Headers", "Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,authorization");
+        response.addHeader("Access-Control-Expose-Headers", "Access-Control-Allow-Origin, Access-Control-Allow-Credentials, authorization");
+        response.addHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,PATCH");
 
-        */
-        String jwt = httpServletRequest.getHeader("Authorization");
-        if(jwt == null) throw new RuntimeException("Not Authorized");
-        /*
-
-         */
-        // sinon
-        filterChain.doFilter(httpServletRequest, httpServletResponse);
+        if (request.getMethod().equals("OPTIONS")) {
+            response.setStatus(HttpServletResponse.SC_OK);
+        } else {
+            // récupérer header
+            String jwtToken = request.getHeader(SecurityParams.JWT_HEADER_NAME);
+            System.out.println("Token = " + jwtToken);
+            // si jwt null ou ne commence pas par prefix, passe au filtre suivant => non authentifié
+            if (jwtToken == null || !jwtToken.startsWith(SecurityParams.HEADER_PREFIX)) {
+                filterChain.doFilter(request, response);
+                return;
+            }
+            // sinon vérifier signature et décoder token
+            JWTVerifier verifier = JWT.require(Algorithm.HMAC256(SecurityParams.SECRET)).build();
+            // j'enlève le prefix
+            String jwt = jwtToken.substring(SecurityParams.HEADER_PREFIX.length());
+            // je vérifie le jwt sans le prefix
+            DecodedJWT decodedJWT = verifier.verify(jwt);
+            System.out.println("JWT = " + jwt);
+            // récupérer username, roles
+            String username = decodedJWT.getSubject();
+            List<String> roles = decodedJWT.getClaims().get("roles").asList(String.class);
+            System.out.println("username = " + username);
+            System.out.println("rôles = " + roles);
+            // ajout des roles à authorities
+            Collection<GrantedAuthority> authorities = new ArrayList<>();
+            roles.forEach(r -> {
+                authorities.add(new SimpleGrantedAuthority(r));
+            });
+            // définit utilisateur pour Spring pour qu'il puisse l'authentifier
+            UsernamePasswordAuthenticationToken user = new UsernamePasswordAuthenticationToken(username, null, authorities);
+            // authentifier utilisateur
+            SecurityContextHolder.getContext().setAuthentication(user);
+            // passer au filtre suivant
+            filterChain.doFilter(request, response);
+        }
     }
 }
