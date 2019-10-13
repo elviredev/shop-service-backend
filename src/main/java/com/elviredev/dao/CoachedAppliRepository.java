7e1bf71a7217548a25c00dd063cbfa0039bf7aee
elviredev <sandrine.rodriguez@hotmail.com>
2019-10-13 19:43:24 +0200

    Update CoachedAppliRepository.java

--- src/main/java/com/elviredev/dao/CoachedAppliRepository.java
+++ src/main/java/com/elviredev/dao/CoachedAppliRepository.java
@@ -2,8 +2,14 @@ package com.elviredev.dao;
 
 import com.elviredev.entities.CoachedAppli;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+
+import java.util.List;
 
 @RepositoryRestResource
 public interface CoachedAppliRepository extends MongoRepository<CoachedAppli, String> {
+    @RestResource(path="/coachedApplisByKeyword")
+    List<CoachedAppli> findByAppliNameContains(@Param("kw") String kw); //http://localhost:8087/coachedApplis/search/coachedApplisByKeyword?kw=AP
 }
