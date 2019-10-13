ceb1095c1b9c500218a8bfac1be4e85dcee688c1
elviredev <sandrine.rodriguez@hotmail.com>
2019-10-13 19:43:21 +0200

    Update ContentAppliRepository.java

--- src/main/java/com/elviredev/dao/ContentAppliRepository.java
+++ src/main/java/com/elviredev/dao/ContentAppliRepository.java
@@ -3,7 +3,12 @@ package com.elviredev.dao;
 import com.elviredev.entities.ContentAppli;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+
+import java.util.List;
 
 @RepositoryRestResource
 public interface ContentAppliRepository extends MongoRepository<ContentAppli, String> {
+    @RestResource(path = "/selectedContents")
+    List<ContentAppli> findBySelectedIsTrue();
 }
